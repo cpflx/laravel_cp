f59c53950ce40649c37f6104ffbbdaaf9a6d49fa
cpflx <547433687@qq.com>
2021-12-28 15:00:39 +0800

    1

--- blog/app/Http/Controllers/api/v1/IndexController.php
+++ blog/app/Http/Controllers/api/v1/IndexController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\api\v1;
 
 
 use App\Http\Controllers\Controller;
+use QL\QueryList;
 
 class IndexController extends Controller
 {
     public function index(){
-        echo 1;
+        $res = QueryList::get('http://www.nipic.com')->find('img')->attrs('src')->toArray();
+        var_dump($res);
     }
 }
